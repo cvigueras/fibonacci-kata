c429d087d3ec5363c708722b2b953142dd8bff08
cvigueras <carlosj.vigueras@gmail.com>
2023-03-30 12:05:04 +0200

    Feat: return 0,1 when given number is one

---
+++ Fibonacci/Fibonacci.Console/Fibonacci.cs
@@ -0,0 +1,18 @@
+namespace Fibonacci.Console;
+
+public class Fibonacci
+{
+    public string GetSequence(int number)
+    {
+        if (number < 0)
+        {
+            throw new InvalidDataException("Negatives numbers are not allowed");
+        }
+
+        if (number == 1)
+        {
+            return "0,1";
+        }
+        return "0";
+    }
+}
