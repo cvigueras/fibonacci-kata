6d93888b12e012e2ce73435f47453c1929b0ead8
cvigueras <carlosj.vigueras@gmail.com>
2023-03-30 11:37:03 +0200

    Test: return_exception_when_number_is_negative

--- Fibonacci/Fibonacci.Test/FibonacciShould.cs
+++ Fibonacci/Fibonacci.Test/FibonacciShould.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace Fibonacci.Test
 {
     public class FibonacciShould
@@ -7,5 +9,21 @@ namespace Fibonacci.Test
         {
         }
 
+        [Test]
+        public void return_exception_when_number_is_negative()
+        {
+            Action action = () => Fibonacci.GetSequence();
+            action.Should().Throw<InvalidDataException>()
+                .WithMessage("Not negatives numbers are allowed");
+        }
+
+    }
+
+    public class Fibonacci
+    {
+        public static void GetSequence()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
